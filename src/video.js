c8c82d4b090210799603f3a3c254f726d4550918
YuriiOL <67682937+YuriiOL@users.noreply.github.com>
2020-10-15 15:46:00 +0300

    add auto play to video

--- src/video.js
+++ src/video.js
@@ -8,6 +8,10 @@ export function flips() {
   const addClass = document.querySelectorAll(".flip-card")
   for (let i = 0; i < addClass.length; i++) {
     addClass[i].classList.add("flips");
+    let video = document.querySelectorAll("video")
+    for (let i = 0; i < video.length; i++) {
+      video[i].play()
+    }
   }
 }
 export function backflips() {
